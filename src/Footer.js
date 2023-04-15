4e7b01f64a32ade423076b7add75c2317866837c
JaishreeNishad <Jaysh2222@gmail.com>
2023-04-15 13:00:21 +0530

    update footer page

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-black flex '>
+        <h1 className='text-white text-sm pb-8 pl-14 font-semibold pt-8 '>© 2023 Lonely Planet, a Red Ventures company. All rights reserved. No part of this site may be reproduced without our written<br/> permission.</h1>
+        <h1 className='text-white font-bold text-sm pt-10 pb-8 ml-10'>LANGUAGE</h1>
+        <button className='text-white mb-8 ml-10 mt-8 border border-gray-500 px-16  py-1 rounded-2xl'>English</button>
+        
+    </div>
+  )
+}
+
+export default Footer
